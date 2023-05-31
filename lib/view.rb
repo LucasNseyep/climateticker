f458700023ddeaf0f7500ab2a90b5eb7a38548b2
Lucas Nseyep <lucas.nseyep@gmail.com>
2023-05-31 11:44:32 +0100

    build: created Gemfile and added tty-prompt gem

--- lib/view.rb
+++ lib/view.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
+require "tty-prompt"
 
 class View
+  def initialize
+    @prompt = TTY::Prompt.new
+  end
+
   def ask_for(item)
-    puts `clear`
-    puts "What #{item} are you looking for?"
-    gets.chomp
+    # puts `clear`
+    # puts "What #{item} are you looking for?"
+    # gets.chomp
+    @prompt.ask("What #{item} are you looking for?")
   end
 
   def ask_for_index
@@ -15,9 +21,11 @@ class View
   def display_list(elements)
     puts `clear`
     elements.delete_at(-1)
-    elements.each_with_index do |element, index|
-      puts "#{index + 1} #{element}"
-    end
+    # elements.each_with_index do |element, index|
+    #   puts "#{index + 1} #{element}"
+    # end
+    p elements.find_index(@prompt.select("Choose", elements)) #we can now return the index immediately
+
   end
 
   def looking_for(element)
@@ -28,3 +36,6 @@ class View
     puts "Retrieving #{element} #{items} ..."
   end
 end
+
+# name = prompt.ask("What is your name?", default: ENV["USER"])
+# puts name
